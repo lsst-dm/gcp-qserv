007cc3706d588ea06565be36e689be74d641dfb3
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-12-29 09:56:14 +0100

    upd

---
+++ env.example.sh
@@ -0,0 +1,4 @@
+PROJECT="qserv-dev-3d7e"
+REGION="us-central1"
+CLUSTER="qserv-dev"
+gcloud config set project $PROJECT
--- resize.example.sh
+++ resize.example.sh
@@ -2,9 +2,8 @@
 
 set -euxo pipefail
 
-PROJECT="qserv-dev-3d7e"
-REGION="us-central1"
-gcloud config set project $PROJECT
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+. $DIR/env.sh
 
 # Size=0
 gcloud container clusters resize qserv-dev --quiet --node-pool czar-pool-b1d5  --region=$REGION --num-nodes 0
